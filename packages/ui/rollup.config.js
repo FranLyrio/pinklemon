83d56f1b5c3721d624282d28782e9aae3b8824d1
Franciele Lyrio <franlyrio@192.168.0.8>
2026-07-22 07:03:16 -0300

    build: altera esrtutura dos arquivos output do rollup

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,19 +1,26 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'index.ts',
-  output: [
-    { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
-    { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
-  ],
-  external: ['react'],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-    }),
-  ],
-};
+export default [
+  {
+    input: 'index.ts',
+    output: [
+      { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
+      { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
+    ],
+    external: ['react'],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json', declaration: false }),
+    ],
+  },
+  {
+    input: 'index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: ['react'],
+    plugins: [dts()],
+  },
+];
